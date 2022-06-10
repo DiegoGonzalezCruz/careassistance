d621c119867070ae8db248e1117f40f2fd4cb1f7
DiegoGonzalezCruz <diego@thinkey.cl>
2022-06-10 13:55:40 -0400

    first sprint

--- components/Boxes/BoxRotatingElements.jsx
+++ components/Boxes/BoxRotatingElements.jsx
@@ -5,7 +5,13 @@ import React from 'react'
 export const BoxRotatingElements = () => {
   return (
     <div className="relative w-screen h-full py-20 bg-primary">
-      <div className="h-full ">{/* <Vector /> */}</div>
+      <div className="h-full ">
+        <Image
+          src={'/images/experiencia_unica/vector2.svg'}
+          layout="fill"
+          className="w-screen"
+        />
+      </div>
     </div>
   )
 }
