44e27200fb00748eb772f2bbb54dbc2b9385c43e
DiegoGonzalezCruz <diego@thinkey.cl>
2022-06-10 13:18:33 -0400

    typos in terms , trabaja, politicas

--- pages/politicas-de-privacidad.js
+++ pages/politicas-de-privacidad.js
@@ -8,8 +8,24 @@ const PoliticasPrivacidad = () => {
           <h1>Políticas de privacidad</h1>
         </div>
         <div className='w-3/4 mx-auto'>
-          <p>That is a remarkable achievement taking into account this month's financial state of things! If all of this comes off as mixed-up to you, that's because it is! Quick: do you have a plan to become cross-media? We think that most efficient web-based applications use far too much XSL, and not enough PNG. It sounds wonderful, but it's 100 percent accurate! The experiences factor is 1000/60/60/24/7/365. If all of this comes off as mixed-up to you, that's because it is! Quick: do you have a virally-distributed plan of action for managing emerging partnerships? Without data hygiene supervising, you will lack architectures. What does the industry jargon 'co-branded' really mean? Is it more important for something to be best-of-breed? The portals factor can be delivered as-a-service to wherever it’s intended to go – mobile. Without micro-resource-constrained performance, you will lack architectures. Quick: do you have a plan to become customized. The capability to implement wirelessly leads to the capacity to harness without devaluing our power to benchmark. The capability to implement wirelessly leads to the awards page of the pudding is in the industry, but our one-to-one, customer-defined, robust C2C2C M&A and user-proof configuration is frequently considered a remarkable achievement taking into account this month's financial state of things! If all of this sounds astonishing to you, that's because it is! A company that can streamline elegantly will (at some unspecified point in the future) be able to transition easily.
-            d</p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis ex arcu, id facilisis neque laoreet nec. Aliquam bibendum consectetur nulla sed egestas. Etiam scelerisque elit suscipit convallis pharetra. Suspendisse quis odio vel lacus maximus dignissim. Vivamus convallis aliquam efficitur. Cras cursus condimentum quam a ultrices. Proin porta libero quis urna varius ultricies. Nunc ullamcorper velit in orci porta feugiat. Maecenas a erat quis odio elementum elementum sed at ante. Duis in vulputate erat. Ut euismod ipsum id ultrices accumsan. Nam non tempor leo, id pellentesque eros. Fusce tincidunt iaculis dolor, eget faucibus lectus eleifend laoreet. Aenean magna lectus, mollis non odio et, eleifend vulputate nunc. Ut a gravida lorem. Suspendisse luctus tortor ut auctor scelerisque.
+          </p>
+
+          <p>
+            Nunc non tincidunt quam. Nam at urna neque. Curabitur lacinia fermentum finibus. Phasellus non tellus arcu. Pellentesque at neque porta, ultricies nisl sed, congue augue. Nullam nisi libero, maximus a elementum id, varius et massa. Mauris dolor tellus, eleifend id augue vitae, volutpat eleifend est. Aliquam vulputate luctus aliquet. Maecenas scelerisque luctus lobortis. Sed vestibulum, metus a pulvinar dictum, diam velit pharetra est, et rhoncus dolor justo nec diam. Donec nec rhoncus diam, eget aliquet leo. Nullam ornare nisl nec massa congue, quis ultricies elit fringilla. Phasellus sed dui accumsan, pretium magna vel, condimentum leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vestibulum ex in urna dignissim elementum. Fusce sodales sem quis est viverra, nec tristique augue sodales.
+          </p>
+
+          <p>
+            Nam bibendum, tortor a dignissim iaculis, dui est molestie massa, suscipit viverra metus eros ac sem. Morbi egestas imperdiet dolor. Morbi porttitor est quis nibh fringilla facilisis. Ut eget turpis ac nunc elementum tristique. Nunc arcu odio, rutrum ac luctus vitae, dapibus sed quam. Proin suscipit nisl a malesuada rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+
+          <p>
+            Aliquam rutrum porta augue, sed ultricies lorem. Integer a ligula leo. Morbi sit amet faucibus tellus. Morbi pretium consectetur nisi, id dictum metus blandit sed. Suspendisse tempus sagittis nisi, eu venenatis tortor congue non. Quisque nec augue vel ex tincidunt tincidunt eget sit amet odio. Aliquam erat erat, pretium sit amet nunc nec, bibendum volutpat nisl. Proin sit amet laoreet enim, ac iaculis odio. Cras vitae condimentum quam.
+          </p>
+
+
         </div>
       </div>
     </div>
